ea3850f231d4102d8b913b29653bd6f8d1688c99
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-10-18 17:05:36 +0200

    make elevator not soft lock you if you stepped off

--- elevator.gd
+++ elevator.gd
@@ -4,25 +4,33 @@ extends Node3D
 @onready var ui = self.get_node("../UI")
 @onready var player = self.get_node("../Player")
 @onready var animation_player = $AnimationPlayer
-
-var dist = 1.6
+@onready var audio_player = $MovingElevator/AudioStreamPlayer3D
+@onready var area = $MovingElevator/Area3D
+@onready var cage = $MovingElevator/Cage
 
 var top = true
 
 var interactable = false
+var moving_up = true
 
 func _process(_delta: float) -> void:
-	#print(visibility.global_position.distance_to(player.global_position), animation_player.current_animation, visibility.is_on_screen())
-	var should_be_interactable = visibility.global_position.distance_to(player.global_position) < dist and not animation_player.is_playing() and visibility.is_on_screen()
+	var should_be_interactable = len(area.get_overlapping_bodies()) != 0 and not animation_player.is_playing() and visibility.is_on_screen()
 	if !interactable and should_be_interactable:
 		ui.show_message("[E] press button")
 	elif interactable and !should_be_interactable:
 		ui.hide_message()
 	interactable = should_be_interactable
+	
+	if moving_up and not animation_player.is_playing():
+		moving_up = false
+		cage.position.y += 100
 
 	if interactable and Input.is_action_just_pressed("interact"):
 		if top:
 			animation_player.play("going_down")
 		else:
 			animation_player.play("going_up")
+			moving_up = true
+			cage.position.y -= 100
 		top = !top
+		audio_player.play()
